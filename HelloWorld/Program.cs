19f3096c0ac7f5244c80809f8576762e0648e360
SHEFFcode <sheff@sheffmachine.com>
2016-09-03 13:41:54 -0700

    Looking into data types in C#

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,18 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var number = 3;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Mosh";
+            var isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
         }
     }
 }
