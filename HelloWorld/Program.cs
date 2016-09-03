12c2747ccb7d48ac83bd9faf9a16c2780d3c86b1
SHEFFcode <sheff@sheffmachine.com>
2016-09-03 15:17:47 -0700

    objects, arrays and strings

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -19,6 +19,29 @@ namespace HelloWorld
             Calculator calculator = new Calculator();
             var result = calculator.Add(1, 2);
             Console.WriteLine(result);
+
+            //Arrays
+            var numbers = new int[3] {1, 2, 3};
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+
+            //Strings
+            var FirstName = "Jeremy";
+            var numbersArray = new int[3] {1, 2, 3};
+            var list = string.Join(",", numbersArray);
+
+            Console.WriteLine(list);
+
+            var line = "Hi There \n folks how is the life going c:\\asdas\\asdsa\\asda";
+            var newLine = @"Hi there
+                            This is a new line 
+                            c:\sadsa\asdas\asdasda";
+
+            Console.WriteLine(line, newLine);
+
         }
     }
 }
