f0d921275fe578024243d6861a86120cef200754
SHEFFcode <sheff@sheffmachine.com>
2016-09-06 08:33:22 -0700

    terciary statement

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,18 +11,33 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int hour = 10;
-            if (hour > 0 && hour < 12)
-            {
-                Console.WriteLine("It's morning!");
-            } else if (hour >= 12 && hour < 18)
-            {
-                Console.WriteLine("It's afternoon!");
-            }
-            else
-            {
-                Console.WriteLine("It's evening!");
-            }
+//            int hour = 10;
+//            if (hour > 0 && hour < 12)
+//            {
+//                Console.WriteLine("It's morning!");
+//            } else if (hour >= 12 && hour < 18)
+//            {
+//                Console.WriteLine("It's afternoon!");
+//            }
+//            else
+//            {
+//                Console.WriteLine("It's evening!");
+//            }
+
+            var isGoldCustomer = true;
+//            float price;
+
+//            if (isGoldCustomer)
+//            {
+//                price = 19.95f;
+//            }
+//            else
+//            {
+//                price = 29.95f;
+//            }
+
+            var price = (isGoldCustomer) ? 19.95f : 29.95f;
+            Console.WriteLine(price);
         }
     }
 }
