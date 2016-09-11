59fd1ee7e96c315134e50568d7d100709361d7a1
SHEFFcode <sheff@sheffmachine.com>
2016-09-11 10:50:17 -0700

    string summary class utility

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Schema;
 using HelloWorld.Math;
 
 namespace HelloWorld
@@ -40,6 +41,35 @@ namespace HelloWorld
 
             Console.WriteLine(priceString);
 
+
+            /*============== Summarizing Text =======================*/
+            var sentence = "This is going to be a really really really long text";
+            const int maxLength = 20;
+
+            if (sentence.Length < maxLength)
+            {
+                Console.WriteLine(sentence);
+            }
+            else
+            {
+                var words = sentence.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxLength)
+                    {
+                        break;
+                    }
+                }
+
+                var summary = String.Join(" ", summaryWords) + "...";
+                Console.WriteLine(summary);
+            }
+
+            SummaryUtility.SummarizeText("This trip");
 
         }
     }
---
+++ HelloWorld/SummaryUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    class SummaryUtility
+    {
+        public static string SummarizeText(string sentence, int maxLength = 20)
+        {
+            /*============== Summarizing Text =======================*/
+            //            var sentence = "This is going to be a really really really long text";
+
+            if (sentence.Length < maxLength)
+            {
+                Console.WriteLine(sentence);
+                return (sentence);
+            }
+            var words = sentence.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                {
+                    break;
+                }
+            }
+
+            var summary = String.Join(" ", summaryWords) + "...";
+            Console.WriteLine(summary);
+            return (summary);
+
+        }
+    }
+}
